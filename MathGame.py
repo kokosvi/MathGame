5b6a5942eedcb638bb551da62acffcd03ac2bc7d
Kaloyan Kostov <kaloyan.kostov.bg@gmail.com>
2020-08-25 09:51:48 +0300

    Initial Commit

---
+++ MathGame.py
@@ -0,0 +1,54 @@
+import os
+from tkinter import *
+import random
+
+root = Tk()
+root.title("Math Game")
+program_directory = sys.path[0]
+root.iconphoto(True, PhotoImage(file=os.path.join(program_directory, "test.png")))
+root.geometry("400x400")
+
+
+def generateNum():
+    return random.randint(0, 100)
+
+
+def calculate():
+    global e
+    global x
+    global y
+    # if (lab1.winfo_exists()):
+    #     lab1.destroy()
+    # if (lab.winfo_exists()):
+    #     lab.destroy()
+    if x + y == int(e.get()):
+        lab = Label(root, text="Correct!")
+        lab.grid(row=4, column=0)
+    else:
+        lab = Label(root, text="Incorrect!")
+        lab.grid(row=4, column=0)
+
+def generate():
+    global x
+    global y
+    x = generateNum()
+    y = generateNum()
+    exercise.destroy()
+    exercise1 = Label(root, text=str(x) + " + " + str(y) + "=", anchor=CENTER, width=30, padx=5, pady=5)
+    exercise1.grid(row=1, column=0, columnspan=2)
+
+
+x = generateNum()
+y = generateNum()
+titlel = Label(root, text="Calculate this:", anchor=CENTER, width=30, padx=5, pady=5)
+titlel.grid(row=0, column=0, columnspan=2)
+exercise = Label(root, text=str(x) + " + " + str(y) + "=", anchor=CENTER, width=30, padx=5, pady=5)
+exercise.grid(row=1, column=0, columnspan=2)
+e = Entry(root)
+e.grid(row=2, column=0, columnspan=2)
+btn_e = Button(root, text="Calculate", command=calculate)
+btn_e.grid(row=3, column=0)
+btn_g = Button(root, text="Generate", command=generate)
+btn_g.grid(row=3, column=1)
+
+root.mainloop()
