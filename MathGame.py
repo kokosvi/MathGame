83ae9a5bc631a169ed2694a89990fe46a549dc58
Kaloyan Kostov <kaloyan.kostov.bg@gmail.com>
2020-08-31 19:00:09 +0300

    fix bugs in calculate and generate functions

--- MathGame.py
+++ MathGame.py
@@ -9,47 +9,43 @@ root.iconphoto(True, PhotoImage(file=os.path.join(program_directory, "test.png")
 root.geometry("400x400")
 
 
-def generateNum():
+def generate_num():
     return random.randint(0, 100)
 
 
 def calculate():
-    global e
-    global x
-    global y
-    # if (lab1.winfo_exists()):
-    #     lab1.destroy()
-    # if (lab.winfo_exists()):
-    #     lab.destroy()
-
-    lab = Label(root)
-    lab.grid(row=4, column=0)
-    if x + y == int(e.get()):
-        lab['text'] = "Correct!"
+    if first_num + second_num == int(your_answer.get()):
+        result_message["text"] = "Correct!"
     else:
-        lab['text'] = "Incorrect!"
+        result_message["text"] = "Incorrect!"
+
 
 def generate():
-    global x
-    global y
-    x = generateNum()
-    y = generateNum()
-    exercise.destroy()
-    exercise1 = Label(root, text=str(x) + " + " + str(y) + " =", anchor=CENTER, width=30, padx=5, pady=5)
-    exercise1.grid(row=1, column=0, columnspan=2)
+    global first_num
+    first_num = generate_num()
+    global second_num
+    second_num = generate_num()
+    sum_expression["text"] = str(first_num) + " + " + str(second_num) + " ="
 
 
-x = generateNum()
-y = generateNum()
-titlel = Label(root, text="Calculate this:", anchor=CENTER, width=30, padx=5, pady=5)
-titlel.grid(row=0, column=0, columnspan=2)
-exercise = Label(root, text=str(x) + " + " + str(y) + " =", anchor=CENTER, width=30, padx=5, pady=5)
-exercise.grid(row=1, column=0, columnspan=2)
-e = Entry(root)
-e.grid(row=2, column=0, columnspan=2)
-btn_e = Button(root, text="Calculate", command=calculate)
-btn_e.grid(row=3, column=0)
-btn_g = Button(root, text="Generate", command=generate)
-btn_g.grid(row=3, column=1)
+title = Label(root, text="Calculate this:", anchor=CENTER, width=30, padx=5, pady=5)
+title.grid(row=0, column=0, columnspan=2)
+
+first_num = generate_num()
+second_num = generate_num()
+sum_expression = Label(root, text=str(first_num) + " + " + str(second_num) + " =", anchor=CENTER, width=30, padx=5, pady=5)
+sum_expression.grid(row=1, column=0, columnspan=2)
+
+your_answer = Entry(root)
+your_answer.grid(row=2, column=0, columnspan=2)
+
+btn_calculate = Button(root, text="Calculate", command=calculate)
+btn_calculate.grid(row=3, column=0)
+
+btn_generate = Button(root, text="Generate", command=generate)
+btn_generate.grid(row=3, column=1)
+
+result_message = Label(root)
+result_message.grid(row=4, column=0)
 
 root.mainloop()
